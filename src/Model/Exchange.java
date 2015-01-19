90b821d72323aa08ee453e7c522db830b1b6c508
Aarón <Aarn@10.225.169.234>
2015-01-19 12:22:11 +0000

    ImageViewer

--- src/Model/Exchange.java
+++ src/Model/Exchange.java
@@ -5,7 +5,7 @@ public class Exchange {
     private Money money;
 
     public void exchange(Money money, ExchangeRate exchangeRate) {
-        //this.money = new Money(money.getAmount().mult(exchangeRate.getRate()), exchangeRate.getToCurrency());
+        this.money = new Money(money.getAmount().mult(exchangeRate.getRate()), exchangeRate.getToCurrency());
     }
 
     public Money getMoney() {
